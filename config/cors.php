69406e1dc8960c07a55f9effa29c219d26940095
Ibrahimmousa1999 <ibrahimmousa19994@gmail.com>
2026-03-09 10:56:09 +0200

    update allowed_origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'https://luxx-store-frontend.vercel.app'],
+    'allowed_origins' => [
+        env('FRONTEND_URL'),
+        'http://localhost:3000',
+        'http://localhost:3001',
+    ],
 
     'allowed_origins_patterns' => [],
 
